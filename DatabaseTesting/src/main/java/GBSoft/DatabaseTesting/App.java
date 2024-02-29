2784b3012343a07ea03c51e03f4e678718d9bba0
GirishBhuj <g.bhujbal@gmail.com>
2024-02-29 16:29:38 +0530

    29-Feb-2024

---
+++ DatabaseTesting/src/main/java/GBSoft/DatabaseTesting/App.java
@@ -0,0 +1,13 @@
+package GBSoft.DatabaseTesting;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
